6da3dc1707b703aa0b39e2d6cab84c397c4b7bd8
Rodrigo Haenggi <rodrigo@codegestalt.com>
2021-03-13 19:44:41 +0100

    fuuuu

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  constraints(host: /^www\./) do
-    get '(*x)' => redirect { |_, request|
-      URI.parse(request.url).tap { |url| url.host.sub!('www.', '') }.to_s
-    }
-  end
-
   constraints(:host => /theminimalistcoder.com/) do
     match "/(*path)" => redirect {|params, req| "https://rodrigohaenggi.com/#{params[:path]}"},  via: [:get, :post]
   end
+
+  match '(*any)', to: redirect(subdomain: ''), via: :all, constraints: {subdomain: 'www'}
 
   root 'articles#index'
   resources :articles, param: :slug, only: %i[index show]
