49eea31861a882d338781d89f3f663f1b3fb2e53
Rodrigo Haenggi <rodrigo@codegestalt.com>
2021-03-13 19:11:21 +0100

    purge tailwind in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,21 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./app/views/**/*.html.erb"],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
   plugins: [
-    require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
     require('postcss-import'),
+    require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
+
+    // only needed if you want to purge
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 }
