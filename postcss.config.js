c170fc7caf22715aafd3ab32a211895d0619e5a9
Rodrigo Haenggi <rodrigo@codegestalt.com>
2021-03-13 20:44:26 +0100

    another purge aproach

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-let environment = {
+module.exports = {
   plugins: [
     require('postcss-import'),
-    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
+    require('tailwindcss')('./tailwind.config.js'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
@@ -11,18 +11,3 @@ let environment = {
     })
   ]
 }
-
-if (process.env.RAILS_ENV === 'production') {
-  environment.plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        './app/**/*.html.erb',
-        './app/helpers/**/*.rb',
-        './app/javascript/**/*.js',
-      ],
-      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
-}
-
-module.exports = environment
